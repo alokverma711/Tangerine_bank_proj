7bd1b920b7b93a1af94cb2c4229f5b4d787856d1
alokverma711 <AlokVerma005@hotmail.com>
2025-12-30 17:50:48 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Earnings",
+    content:
+      "Wealth can open doors, but you decide which ones to walk through.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Move Your Balance",
+    content:
+      "Switch your balance effortlessly for better rates.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Guaranteed Security",
+    content:
+      "Money eases life’s challenges, and those who have it are fortunate.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money serves as a vehicle for your ambitions, yet the steering wheel always remains in your hands.",
+    name: "Sam",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Life gets easier with money, and those who have it are fortunate.",
+    name: "Tony",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Real wealth is often found among those in finance and international trade",
+    name: "Jack",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Backed by Trusted Companies",
+    value: "500+",
+  },
+  {
+    id: "stats-2",
+    title: "Transaction",
+    value: "999+",
+  },
+  {
+    id: "stats-3",
+    title: "Secured Users",
+    value: "50k+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.Tangerine.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.Tangerine.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.Tangerine.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.Tangerine.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.Tangerine.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.Tangerine.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.Tangerine.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.Tangerine.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.Tangerine.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.Tangerine.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.Tangerine.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.Tangerine.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
